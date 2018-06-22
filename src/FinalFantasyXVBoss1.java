97469c010330abf109b6ef91068ecf2409839de6
GamesInsane <36931472+GamesInsane@users.noreply.github.com>
2018-06-22 10:06:28 -0400

    Update FinalFantasyXVBoss1.java

--- src/FinalFantasyXVBoss1.java
+++ src/FinalFantasyXVBoss1.java
@@ -4,9 +4,9 @@ import java.util.Collections;
 import java.util.*;
 
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * author Tom Hoogkamp
+ * no return statements
+ * june 22, 2018
  */
 
 /**
@@ -20,7 +20,7 @@ public class FinalFantasyXVBoss1 extends javax.swing.JFrame {
      */
     public FinalFantasyXVBoss1() {
         initComponents();
-//        ArrayList <String> names = new ArrayList();
+//    ArrayList <String> names = new ArrayList();
 //    ArrayList <String> levels = new ArrayList();
 //    ArrayList <String> healths = new ArrayList();
 //    ArrayList <String> strengths = new ArrayList();
@@ -29,12 +29,12 @@ public class FinalFantasyXVBoss1 extends javax.swing.JFrame {
 //    ArrayList <String> playerLevels = new ArrayList();
 //    
 //    Collections.addAll(names, "Arachne", "Aramusha", "Ardyn", "Bandarsnatch", "Behemoth King", "Bennu", "Bilröst", "Bloodhorn", "Catoblepas", "Chandravarma", "Deathclaw", "Ifrit", "Jabberwock", "Jormungand", "Kengo", "Lakhmu Flan", "Leviathan", "MA-X Cuirass", "MA-X Dux", "MA-X Maniple", "Mahanaga", "Malbodoom", "Malboro", "Manxom", "Melusine", "Mictlantecihuatl", "Naga", "Naglfar", "Omega", "Phalaris", "Quetzalcoatl", "Ravus", "Rogue Behemoth", "Rogue Karlabos", "The Mystic", "Titan", "Treant");
-//        Collections.addAll(levels, "12", "50 - 52", "58", "38 - 47", "42", "55", "99", "2", "38", "73", "34", "46", "58", "50", "71", "61", "42", "22 - 51", "51", "23 - 55", "78", "85", "38", "94", "99", "90", "30", "120", "119", "85", "32", "44", "35", "35", "53", "25", "38");
-//        Collections.addAll(healths, "13,000", "39,500 - 175,600", "200,000", "75,800 - 141,200", "120,900", "198,300", "649,700", "9,100", "85,500", "285,600", "72,300", "99,500", "201,200", "184,200", "290,200", "274,000", "290,000", "46,200 - 108,500", "108,500", "47,700 - 124,900", "380,800", "136,800", "35,000", "483,700", "1,080,520", "457,900", "30,700", "875,700", "555,300", "316,800", "64,600", "275,000", "69,800", "75,300", "147,500", "213,000", "71,000");
-//        Collections.addAll(strengths, "780", "12,120 - 27,710", "4,760", "8,160 - 21,020", "13,720", "28,800", "114,210", "240", "12,060", "56,980", "4,030", "18,070", "36,680", "22,940", "58,250", "39,180", "3,070", "2,240 - 19,290", "19,290", "2,360 - 23,370", "72,310", "51,440", "6,180", "105,320", "65,820", "91,070", "3,400", "153,380", "147,920", "61,150", "4,840", "8,000", "5,990", "6,350", "18,931", "580", "7,160");
-//        Collections.addAll(weaknesss, "Greatswords, Machinery, Fire, Light", "Swords, Daggers, Lightning, Light", "Nothing", "Polearms, Machinery, Lightning", "Greatswords, Shields", "Polearms, Machinery, Lightning", "Swords, Daggers, Ice", "Shields, Polearms, Fire", "Polearms, Machinery, Ice", "Light", "Shields, Machinery, Fire, Light", "Ice", "Polearms, Machinery, Ice", "Swords, Daggers, Ice", "Polearms, Firearms, Lightning, Light", "Light", "Nothing", "Shields, Machinery", "Shields, Machinery", "Shields, Machinery", "Greatswords, Daggers, Ice", "Greatswords, Machinery, Fire", "Greatswords, Machinery, Fire", "Polearms, Lightning", "Fire, Light", "Polearms, Daggers, Ice", "Greatswords, Daggers, Ice", "Fire, Ice, Lightning, Light, Poison, Stop", "Lightning", "Greatswords, Shields, Fire", "Polearms, Daggers, Ice", "Light", "Greatswords, Shields", "Swords, Polearms, Ice", "Fire, Light", "Nothing", "Greatswords, Machinery, Fire");
-//        Collections.addAll(resistants, "Lightning", "Fire", "Everything", "Light, Fire, Death, Alterna, Stop", "Ice, Death, Alterna, Stop", "Light, Ice, Death, Alterna, Poison, Stop", "Fire, Death, Alterna, Stop", "Ice, Death, Stop", "Lightning, Light, Stop, Death, Alterna", "Fire, Ice, Lightning, Swords, Greatswords, Shields, Polearms, Daggers, Firearms, Machinery", "Ice, Death , Alterna", "Fire, Death, Alterna, Poison, Stop", "Fire, Death, Alterna, Stop", "Fire, Death, Alterna, Stop", "Fire, Death, Alterna", "Swords, Greatswords, Shields, Polearms, Daggers, Firearms, Machinery, Fire, Ice, Lightning, Death", "Swords, Greatswords, Shields, Polearms, Daggers, Firearms, Machinery, Fire, Ice, Lightning, Light, Death, Alterna, Poison, Stop", "Light, Death, Alterna, Poison, Stop", "Light, Death, Alterna, Poison, Stop", "Light, Death, Alterna, Poison, Stop", "Fire, Light, Death, Alterna, Stop", "Ice, Light, Death, Alterna", "Ice, Light, Death, Alterna", "Greatswords, Ice, Light, Stop, Death", "Ice, Lightning", "Lightning, Death, Alterna, Stop", "Fire, Light, Death", "Swords, Greatswords, Shields, Polearms, Daggers, Firearms, Machinery, Death, Alterna", "Light, Swords, Greatswords, Shields, Polearms, Daggers, Firearms, Machinery, Fire, Ice, Death, Alterna, Poison, Stop", "Ice, Light, Death, Alterna", "Lightning, Death, Alterna, Stop", "Fire, Ice, Lightning, Death, Alterna, Poison, Stop", "Fire, Light, Alterna, Stop, Death", "Fire, Light, Death", "Ice, Lightning, Death, Alterna, Poison, Stop", "Death, Alterna, Poison, Stop", "Lightning, Light, Death, Alterna");
-//        Collections.addAll(playerLevels, "11 - 14", "48 - 54", "60 - 65", "38 - 50", "40 - 43", "54 - 57", "99 - 103", "3 - 4", "38 - 40", "74 - 77", "35 - 37", "48 - 52", "60 - 63", "53 - 55", "73 - 77", "63 - 66", "44 - 48", "24 - 56", "53 - 56", "26 - 60", "80 - 84", "87 - 92", "40 - 43", "95 - 98", "99 - 103", "90 - 95", "28 - 34", "118 - 120", "118 - 120", "84 - 90", "30 - 34", "43 - 48", "34 - 38", "34 - 38", "52 - 58", "24 - 30", "37 - 43");
+//    Collections.addAll(levels, "12", "50 - 52", "58", "38 - 47", "42", "55", "99", "2", "38", "73", "34", "46", "58", "50", "71", "61", "42", "22 - 51", "51", "23 - 55", "78", "85", "38", "94", "99", "90", "30", "120", "119", "85", "32", "44", "35", "35", "53", "25", "38");
+//    Collections.addAll(healths, "13,000", "39,500 - 175,600", "200,000", "75,800 - 141,200", "120,900", "198,300", "649,700", "9,100", "85,500", "285,600", "72,300", "99,500", "201,200", "184,200", "290,200", "274,000", "290,000", "46,200 - 108,500", "108,500", "47,700 - 124,900", "380,800", "136,800", "35,000", "483,700", "1,080,520", "457,900", "30,700", "875,700", "555,300", "316,800", "64,600", "275,000", "69,800", "75,300", "147,500", "213,000", "71,000");
+//    Collections.addAll(strengths, "780", "12,120 - 27,710", "4,760", "8,160 - 21,020", "13,720", "28,800", "114,210", "240", "12,060", "56,980", "4,030", "18,070", "36,680", "22,940", "58,250", "39,180", "3,070", "2,240 - 19,290", "19,290", "2,360 - 23,370", "72,310", "51,440", "6,180", "105,320", "65,820", "91,070", "3,400", "153,380", "147,920", "61,150", "4,840", "8,000", "5,990", "6,350", "18,931", "580", "7,160");
+//    Collections.addAll(weaknesss, "Greatswords, Machinery, Fire, Light", "Swords, Daggers, Lightning, Light", "Nothing", "Polearms, Machinery, Lightning", "Greatswords, Shields", "Polearms, Machinery, Lightning", "Swords, Daggers, Ice", "Shields, Polearms, Fire", "Polearms, Machinery, Ice", "Light", "Shields, Machinery, Fire, Light", "Ice", "Polearms, Machinery, Ice", "Swords, Daggers, Ice", "Polearms, Firearms, Lightning, Light", "Light", "Nothing", "Shields, Machinery", "Shields, Machinery", "Shields, Machinery", "Greatswords, Daggers, Ice", "Greatswords, Machinery, Fire", "Greatswords, Machinery, Fire", "Polearms, Lightning", "Fire, Light", "Polearms, Daggers, Ice", "Greatswords, Daggers, Ice", "Fire, Ice, Lightning, Light, Poison, Stop", "Lightning", "Greatswords, Shields, Fire", "Polearms, Daggers, Ice", "Light", "Greatswords, Shields", "Swords, Polearms, Ice", "Fire, Light", "Nothing", "Greatswords, Machinery, Fire");
+//    Collections.addAll(resistants, "Lightning", "Fire", "Everything", "Light, Fire, Death, Alterna, Stop", "Ice, Death, Alterna, Stop", "Light, Ice, Death, Alterna, Poison, Stop", "Fire, Death, Alterna, Stop", "Ice, Death, Stop", "Lightning, Light, Stop, Death, Alterna", "Fire, Ice, Lightning, Swords, Greatswords, Shields, Polearms, Daggers, Firearms, Machinery", "Ice, Death , Alterna", "Fire, Death, Alterna, Poison, Stop", "Fire, Death, Alterna, Stop", "Fire, Death, Alterna, Stop", "Fire, Death, Alterna", "Swords, Greatswords, Shields, Polearms, Daggers, Firearms, Machinery, Fire, Ice, Lightning, Death", "Swords, Greatswords, Shields, Polearms, Daggers, Firearms, Machinery, Fire, Ice, Lightning, Light, Death, Alterna, Poison, Stop", "Light, Death, Alterna, Poison, Stop", "Light, Death, Alterna, Poison, Stop", "Light, Death, Alterna, Poison, Stop", "Fire, Light, Death, Alterna, Stop", "Ice, Light, Death, Alterna", "Ice, Light, Death, Alterna", "Greatswords, Ice, Light, Stop, Death", "Ice, Lightning", "Lightning, Death, Alterna, Stop", "Fire, Light, Death", "Swords, Greatswords, Shields, Polearms, Daggers, Firearms, Machinery, Death, Alterna", "Light, Swords, Greatswords, Shields, Polearms, Daggers, Firearms, Machinery, Fire, Ice, Death, Alterna, Poison, Stop", "Ice, Light, Death, Alterna", "Lightning, Death, Alterna, Stop", "Fire, Ice, Lightning, Death, Alterna, Poison, Stop", "Fire, Light, Alterna, Stop, Death", "Fire, Light, Death", "Ice, Lightning, Death, Alterna, Poison, Stop", "Death, Alterna, Poison, Stop", "Lightning, Light, Death, Alterna");
+//    Collections.addAll(playerLevels, "11 - 14", "48 - 54", "60 - 65", "38 - 50", "40 - 43", "54 - 57", "99 - 103", "3 - 4", "38 - 40", "74 - 77", "35 - 37", "48 - 52", "60 - 63", "53 - 55", "73 - 77", "63 - 66", "44 - 48", "24 - 56", "53 - 56", "26 - 60", "80 - 84", "87 - 92", "40 - 43", "95 - 98", "99 - 103", "90 - 95", "28 - 34", "118 - 120", "118 - 120", "84 - 90", "30 - 34", "43 - 48", "34 - 38", "34 - 38", "52 - 58", "24 - 30", "37 - 43");
 //
 //    
 //        bossName.setText( names.get(names.size()));
@@ -342,7 +342,7 @@ public class FinalFantasyXVBoss1 extends javax.swing.JFrame {
      * regenerated by the Form Editor.
      */
     @SuppressWarnings("unchecked")
-    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">                          
     private void initComponents() {
 
         jPanel1 = new javax.swing.JPanel();
@@ -439,12 +439,16 @@ public class FinalFantasyXVBoss1 extends javax.swing.JFrame {
         );
 
         pack();
-    }// </editor-fold>//GEN-END:initComponents
+    }// </editor-fold>                        
      
-    private void backToListActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_backToListActionPerformed
+    private void backToListActionPerformed(java.awt.event.ActionEvent evt) {                                           
+       /**
+        * the following code will move the user back to the boss selection
+        * menu for them to pick another boss
+        */
         this.setVisible(false);
         new FinalFantasyXVBossList().setVisible(true);
-    }//GEN-LAST:event_backToListActionPerformed
+    }                                          
     /**
      * @param args the command line arguments
      */
@@ -482,7 +486,7 @@ public class FinalFantasyXVBoss1 extends javax.swing.JFrame {
         });
     }
 
-    // Variables declaration - do not modify//GEN-BEGIN:variables
+    // Variables declaration - do not modify                     
     private javax.swing.JButton backToList;
     private javax.swing.JLabel bossName;
     private javax.swing.JLabel health;
@@ -492,5 +496,5 @@ public class FinalFantasyXVBoss1 extends javax.swing.JFrame {
     private javax.swing.JLabel resistant;
     private javax.swing.JLabel strength;
     private javax.swing.JLabel weakness;
-    // End of variables declaration//GEN-END:variables
+    // End of variables declaration                   
 }
